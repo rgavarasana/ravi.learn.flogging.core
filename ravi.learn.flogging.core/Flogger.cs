28daf566c82d78814a87fff6d01e6563b15319f7
Ravi Gavarasana <gavarasana@gmail.com>
2019-01-21 09:21:13 -0500

    Add project files.

---
+++ ravi.learn.flogging.core/Flogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog;
+
+namespace ravi.learn.flogging.core
+{
+    public static class Flogger
+    {
+        private static readonly ILogger _perfLogger;
+        private static readonly ILogger _usageLogger;
+        private static readonly ILogger _errorLogger;
+        private static readonly ILogger _diagnosticLogger;
+
+        static Flogger()
+        {
+            _perfLogger = new LoggerConfiguration()
+                .WriteTo.File(path: "d:\\temp\\logs\\perf.log")
+                .CreateLogger();
+            _usageLogger = new LoggerConfiguration()
+                .WriteTo.File(path: "d:\\temp\\logs\\perf.log")
+                .CreateLogger();
+            _errorLogger = new LoggerConfiguration()
+                .WriteTo.File(path: "d:\\temp\\logs\\perf.log")
+                .CreateLogger();
+            _diagnosticLogger = new LoggerConfiguration()
+                .WriteTo.File(path: "d:\\temp\\logs\\perf.log")
+                .CreateLogger();
+        }
+    }
+}
---
+++ ravi.learn.flogging.core/dto/FlogDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ravi.learn.flogging.core.dto
+{
+    public class FlogDetail
+    {
+        public FlogDetail()
+        {
+            TimeStamp = DateTime.UtcNow;
+        }
+
+        public DateTime TimeStamp { get; }
+        public string Message { get; set; }
+        public string Product { get; set; }
+        public string Layer { get; set; }
+        public string Location { get; set; }
+        public string HostName { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public long? ElapsedTimeInMilliseconds { get; set; }
+        public string CorrelationId { get; set; }
+        public Dictionary<string, object> AdditionalInfo { get; set; }
+
+    }
+}
